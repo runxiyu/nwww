81b364140d421d160eaf0fcb865858455971c26e
Runxi Yu <me@runxiyu.org>
2025-01-09 20:12:27 +0800

    Don't knuth the chinese

--- static/linebreak_load.js
+++ static/linebreak_load.js
@@ -2,7 +2,7 @@ function typesetLineBreaks() {
 	console.log("Typesetting line breaks...");
 	const lib = window.texLineBreak_lib;
 	const h = lib.createHyphenator(window["texLineBreak_hyphens_en-us"]);
-	const paras = [...document.querySelectorAll("p")].filter(p => !p.closest(".poem"));
+	const paras = [...document.querySelectorAll("p")].filter(p => !p.closest(".poem") && !p.closest(".chinese"));
 	lib.justifyContent(paras, h);
 	console.log("Done typesetting line breaks.");
 }
